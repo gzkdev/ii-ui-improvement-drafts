17f3660a93153f6f61642b5ddb46cdb947cd078a
Ezihe Godswill <ezihegodswill01@gmail.com>
2025-06-11 20:41:47 +0100

    init app

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="py-10 px-6"></div>;
+}
---
+++ src/constants/navbar.ts
@@ -0,0 +1,44 @@
+export const NAVBAR_LINKS = [
+  {
+    label: "Trade",
+    value: "/",
+  },
+  {
+    label: "Prices",
+    value: "/",
+    options: [
+      {
+        label: "Options | Digital Options | Forwards",
+        value: "/",
+      },
+      {
+        label: "Structured Products",
+        value: "/",
+      },
+      {
+        label: "Linear Combinations",
+        value: "/",
+      },
+    ],
+  },
+  {
+    label: "Dashboard",
+    value: "/",
+  },
+  {
+    label: "Analytics",
+    value: "/",
+  },
+  {
+    label: "Airdrop",
+    value: "/",
+  },
+  {
+    label: "Staking",
+    value: "/",
+  },
+  {
+    label: "AI Agents",
+    value: "/",
+  },
+];
